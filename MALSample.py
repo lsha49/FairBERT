77a510ede32d177ab485f4c22299cbf234d9d0b0
lele sha <shahaoxiang@gmail.com>
2022-05-07 13:15:14 +1000

    c

--- MALSample.py
+++ MALSample.py
@@ -42,8 +42,8 @@ from scipy.spatial import distance
 
 # forum_2021_lang_train_embed_bert_base
 # forum_2021_lang_unselected_sample
-Corpus = pd.read_csv('../../uq67_scratch/bfiledata/forum_2021_lang_train_embed_bert_base.csv', encoding='latin-1')
-selectSamplesGroup1 = 5000
+Corpus = pd.read_csv('../../uq67_scratch/bfiledata/forum_2021_lang_unselected_sample.csv', encoding='latin-1')
+selectSamplesGroup1 = 10000
 selectSamplesGroup0 = 5000
 
 
